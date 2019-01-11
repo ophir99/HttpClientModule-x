da2e0d810577d84b271c08e4067c0c83e78e3e1b
kavya.pl <kavya.pl@gmail.com>
2019-01-11 20:18:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'http-awesome';
+  title = "http-awesome";
+  data = [];
+  imgUrl = [];
+  myForm;
+  dogGetReq = this.http.get("https://dog.ceo/api/breeds/image/random/30");
+  constructor(private http: HttpClient, private fBuilder: FormBuilder) {
+    this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe(
+      data => {
+        console.log(data);
+        this.data = data;
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        console.log("Request is processed");
+      }
+    );
+
+    this.dogGetReq.subscribe(
+      data => {
+        console.log(data);
+        this.imgUrl = data.message;
+      },
+      err => {
+        console.log(err);
+      },
+      () => {}
+    );
+
+    this.myForm = this.fBuilder.group({
+      title: [],
+      body: []
+    });
+  }
+
+  sendData() {
+    this.http
+      .post("https://jsonplaceholder.typicode.com/posts", this.myForm.value)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
 }
